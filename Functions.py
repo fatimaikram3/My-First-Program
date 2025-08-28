75e2a80d316f1e3a81fb4f5e5a41ca38f267f954
Fatima Ikram <fatimaikram3127@gmail.com>
2025-08-28 11:08:15 -0700

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,16 @@
+#Functions in python
+
+def greet():
+    print("Hello! welcome to python.")
+#calling the function
+greet()
+
+# function with parameters
+def greet_user(name):
+    print (f"hello,{name}!Nice to meet you")
+#calling function
+greet_user("Fatima")
+
+def greet(name):
+    print(f"Assalam o Alaikum,{name}!How are you?")
+greet("Fatima")
