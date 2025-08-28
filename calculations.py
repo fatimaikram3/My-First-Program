17de0fed4ffcc00c383d3d5ad05c28b056b67464
Fatima Ikram <fatimaikram3127@gmail.com>
2025-08-28 11:03:23 -0700

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,17 @@
+#simple calculations
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("sum:",a+b)
+
+#2 
+a=int(input("Enter the first digit:"))
+b=int(input("Enter the second digit:"))
+print("sum:",a-b)
+
+#3 EVEN OR ODD
+num=int(input("Enter a number:"))
+if num%2==0:
+    print ("The number is even")
+else:
+    print("The number is odd")
+
